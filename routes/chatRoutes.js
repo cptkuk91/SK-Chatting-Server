190cd530678fcde1a34d11a111a6a1a487e23ad0
cptkuk91 <cptkuk91@gmail.com>
2024-01-20 22:29:12 +0900

    feat: chat server setting

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const chatController = require("../controllers/chatController");
 
-router.get("/:user1Id/:user2Id", chatController.getTwoUserChat);
+router.get("/:user1Id/:user2Id", chatController.getTwoUserChat); // 1 대 1 채팅
+// 모두가 모이는 채팅
+// 랜덤 채팅
 
 module.exports = router;
