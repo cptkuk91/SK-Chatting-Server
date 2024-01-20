c50e99b0a84c1fe4d06d754d3680d28120020d78
cptkuk91 <cptkuk91@gmail.com>
2024-01-20 19:26:56 +0900

    feat: simple chat function

---
+++ models/Chat.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
